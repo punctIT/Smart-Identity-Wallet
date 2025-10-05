7437e59879cb54e7c3478b46d7bfd52bcf6b1779
punctIT <teodorivan2005@gmail.com>
2025-10-05 18:57:45 +0300

    concurent server + client for testing

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,12 @@
+mod network;
+
+use crate::network::server_configure::ServerConfig;
+
 fn main() {
-    println!("Hello, world!");
+    let mut server: ServerConfig = ServerConfig::new();
+    server
+        .set_ip(String::from("0.0.0.0"))
+        .set_port(1234)
+        .bind_and_listen()
+        .accept_connection();
 }
---
+++ test_client/target/debug/deps/test_client.d
@@ -0,0 +1,5 @@
+C:\Users\teodo\Desktop\Smart Identity Wallet\test_client\target\debug\deps\test_client.exe: src\main.rs
+
+C:\Users\teodo\Desktop\Smart Identity Wallet\test_client\target\debug\deps\test_client.d: src\main.rs
+
+src\main.rs:
