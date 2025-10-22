99cedd08f93211e129bdc5b027c1a5ee63ed8202
punctIT <teodorivan2005@gmail.com>
2025-10-22 11:56:57 +0300

    save

--- ai_service/chat_bot.py
+++ ai_service/chat_bot.py
@@ -1,11 +1,16 @@
+from dotenv import load_dotenv
+import os
 
 
 
 class ChatBot:
     def __init__(self):
-        pass
+        load_dotenv()  # Dacă fișierul nu e în același folder, specifică path-ul: load_dotenv("cale/catre/.env")
+        self.api_ai = os.getenv("API_AI")
     def get_response(self,text):
         if text == "salut":
             return "ana re mere msau cevaana re mere msau cevaana re mere msau cevaana re mere msau cevaana re mere msau cevaana re mere msau cevaana re mere msau cevaana re mere msau cevaana re mere msau cevaana re mere msau cevaana re mere msau cevaana re mere msau cevaana re mere msau cevaana re mere msau cevaana re mere msau ceva"
+        elif text =="api":
+            return self.api_ai 
         else :
             return "nu m ai salutat"
